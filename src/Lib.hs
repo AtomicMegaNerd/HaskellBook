589d8edf830317d4df37d2dd3d076dae2c4e46d5
Chris Dunphy <rc.dunphy@gmail.com>
2017-01-28 16:13:05 -0700

    Did some more lessons

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      sayHello
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+sayHello :: String -> IO ()
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+adder :: Num a => a -> a -> a
+adder x y = x + y
+
+triple :: Num a => a -> a
+triple x = x * 3
+
+multByPi :: Floating a => a -> a
+multByPi x = pi * x
