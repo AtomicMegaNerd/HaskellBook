a795e1318842ccac1a315677d5acf4433460b8ba
Chris Dunphy <rc.dunphy@gmail.com>
2017-02-03 15:23:55 -0700

    Chapter 3 is done.
    I have finished Chapter 3 of this book and now I am moving on.

---
+++ src/Ch3Exer.hs
@@ -0,0 +1,29 @@
+module Ch3Exer where
+
+exclam :: [Char] -> [Char]
+exclam s = s ++ "!"
+
+getLastLetterInFirstWord :: [Char] -> [Char]
+getLastLetterInFirstWord s = drop (l - 1) fw
+    where l = length fw
+          fw = wds !! 0
+          wds = words s
+
+getLastWord :: [Char] -> [Char]
+getLastWord s = w !! (length w - 1)
+    where w = words s
+
+thirdLetter :: [Char] -> Char
+thirdLetter s = s !! 2
+
+letterIndex :: Int -> Char
+letterIndex x = s !! x
+    where s = "Curry is awesome!"
+
+main :: IO ()
+main = do
+    print $ exclam "Curry is awesome"
+    print $ getLastLetterInFirstWord "Curry is awesome!"
+    print $ getLastWord "Curry is awesome!"
+    print $ thirdLetter "Curry is awesome!"
+    print $ letterIndex 5
