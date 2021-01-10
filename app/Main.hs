41f015f866da287e8b0ef89925c0f660b02c2069
Chris Dunphy <chris@megaparsec.ca>
2021-01-10 08:13:45 -0700

    Re-did Ch6 Eq typeclass exercises

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,5 @@
 module Main where
 
-import Ch7Exer2
-
-str1 :: [Char]
-str1 = "Hello"
-
-str2 :: [Char]
-str2 = "pannap"
-str3 :: [Char]
-
-str3 = "roflfor"
-
 main :: IO ()
 main = do
   putStrLn "Hello, world"
