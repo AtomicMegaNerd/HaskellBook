68b69f4e568c6d0dd299c4cd9d20385d62b6c35d
Chris Dunphy <chris@megaparsec.ca>
2021-01-27 18:58:34 -0700

    This is using an older GHC
    We reverted to an older version of ghc so that our VSCode plug-in still works.

--- src/Arith3Broken.hs
+++ src/Arith3Broken.hs
@@ -1,6 +1,16 @@
 -- arith3broken.hs
 module Arith3Broken where
 
+data RcdDate = Mon | Tues | Wed | Thurs | Fri | Sat | Sun deriving (Show, Eq, Ord)
+
+missing_case :: Num p => RcdDate -> p
+missing_case Mon = 1
+missing_case Tues = 2
+missing_case Wed = 3
+missing_case Thurs = 4
+missing_case Sat = 5
+missing_case Sun = 6
+
 main :: IO ()
 main = do
   print $ 1 + 2
