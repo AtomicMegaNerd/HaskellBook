a4136797fc932cc47037d4ca4f914b2f31c08de7
Chris Dunphy <chris@megaparsec.ca>
2021-01-27 19:12:37 -0700

    Brain starting to rot, got a bit done

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,17 @@
 module Main where
 
 import Factorial
+import Ch8RecursiveFun
 
 main :: IO ()
 main = do
-  let x = factorial 5
+  let x = factorial 5 :: Integer
   print x
+
+  let y = incTimes 5 5 :: Integer
+  print y
+
+  let z = incTimes' 10 10 :: Integer
+  print z
+
   putStrLn "Program complete"
