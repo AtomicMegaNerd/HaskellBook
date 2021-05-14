354400e6a5368c1a81ec7d8c16322c0c77f4f9ff
Chris Dunphy <chris@megaparsec.ca>
2021-05-13 20:08:52 -0600

    Got more exercises working from Chapter 11

--- src/Ch11Exers.hs
+++ src/Ch11Exers.hs
@@ -1,5 +1,7 @@
 module Ch11Exers where
 
+import Data.Char
+
 -- As-patterns are an interesting idea that let's you both refer to something as a whole
 -- as well as a part of that same something.
 
@@ -30,3 +32,66 @@ isSubseqOf _ [] = False
 isSubseqOf sub@(x : xs) (y : ys)
   | x == y = isSubseqOf xs ys
   | otherwise = isSubseqOf sub ys
+
+-- This function takes a string and splits that string by a character that we provide which
+-- can be a period, a comma, a space, or anything else we want.  This may be a bit grotesque
+-- but I am still new at Haskell and it certainly works!
+splitStringByChar :: Char -> String -> [String]
+splitStringByChar _ [] = []
+splitStringByChar ch sent = fst res : splitStringByChar ch (snd res)
+  where
+    res:: (String, String)
+    res = mkWord sent
+    mkWord :: String -> (String, String) 
+    mkWord sent = mkWordInner sent ([],[])
+      where
+        mkWordInner :: String -> (String, String) -> (String, String)
+        mkWordInner [] buffer = buffer
+        mkWordInner (x:xs) (word, _)
+          | x == ch = (word, xs)
+          | otherwise = mkWordInner xs (word ++ [x], xs)
+
+joinStringsByChar :: Char -> [String] -> String 
+joinStringsByChar ch stringList = joinInner stringList []
+  where
+    joinInner :: [String] -> String -> String
+    joinInner [] buffer = buffer
+    joinInner (x:xs) buffer = x ++ [ch] ++ joinInner xs buffer
+
+-- I can use my splitStringByChar function to define my own version of the words function.
+rcdWords :: String -> [String]
+rcdWords = splitStringByChar ' '
+
+splitSentences :: String -> [String]
+splitSentences = splitStringByChar '.'
+
+joinSentences :: [String] -> String 
+joinSentences = joinStringsByChar '.'
+
+-- This function capitalizes the first letter of every word.
+capitalizeWord :: String -> String 
+capitalizeWord [] = []
+capitalizeWord (x:xs) = toUpper x : xs
+
+-- This function splits a sentence into words and returns a tuple of each word both 
+-- capitalized and not capitalized.
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords sent = map cAndT (rcdWords sent)
+  where
+    cAndT :: String -> (String, String)
+    cAndT orig@(x:xs) = (orig, toUpper x : xs)
+
+-- This function will capitalize the first word in each sentence in a paragraph.  This is really
+-- ugly because we have to account for spaces.
+capitalizeParagraph :: String -> String 
+capitalizeParagraph = joinSentences . map capitalizeFirstLetterInAString . splitSentences
+  where
+    capitalizeFirstLetterInAString :: String -> String
+    capitalizeFirstLetterInAString s = inner s []
+      where
+        inner :: String -> String -> String
+        inner [] buffer = buffer
+        inner orig@(x:xs) buffer
+          | isLetter x = buffer ++ capitalizeWord orig
+          | otherwise = inner xs (x:buffer)
+
